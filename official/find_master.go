8fcfe8efaaf4c3f2a4055dd021f8074ba6f5b93d
ogame-ninja <45097930+ogame-ninja@users.noreply.github.com>
2024-06-09 22:28:19 -0700

    Update find_master.go

--- official/find_master.go
+++ official/find_master.go
@@ -1,14 +1,15 @@
 func findCelestialWithHigherFleetValue() {
+    lfBonuses, _ = GetCachedLfBonuses()
     fleets, _ = GetFleets()
     master = nil
     maxVal = 0
     for celestial in GetCachedCelestials() {
         ships, _ = celestial.GetShips()
-        value = ships.FleetValue()
+        value = ships.FleetValue(lfBonuses)
         coord = celestial.GetCoordinate()
         for fleet in fleets {
             if (fleet.Origin.Equal(coord) && fleet.Mission != PARK) || (fleet.Destination.Equal(coord) && fleet.Mission == PARK) {
-                value += fleet.Ships.FleetValue()
+                value += fleet.Ships.FleetValue(lfBonuses)
             }
         }
         if value > maxVal {
