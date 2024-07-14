22dc0f24c4e146d92c81f080bfb9b23fc7ab9ed0
ogame-ninja <45097930+ogame-ninja@users.noreply.github.com>
2024-07-14 00:23:23 -0700

    Create debris_field_watcher.go

---
+++ official/go_vm/debris_field_watcher.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "nja"
+)
+
+func main() {
+    debrisCoord, _ := nja.ParseCoord("D:1:111:11")
+    delaySecs := int64(2)
+    for {
+        systemInfo, _ := nja.GalaxyInfos(debrisCoord.Galaxy, debrisCoord.System)
+        planetInfo := systemInfo.Position(debrisCoord.Position)
+        if planetInfo == nil {
+            panic("planet not found")
+        }
+        if planetInfo.Debris.RecyclersNeeded == 0 {
+            hour, min, sec := nja.Clock()
+            msg := fmt.Sprintf("Debris field is gone at %dh%dm%d", hour, min, sec)
+            nja.SendTelegram(nja.TELEGRAM_CHAT_ID, msg)
+            break
+        }
+        nja.Sleep(delaySecs * 1000)
+    }
+    println("Debris field script exit")
+}
