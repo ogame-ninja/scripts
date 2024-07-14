bc6911e4256ec4c9d4342056f485344fba93562b
ogame-ninja <45097930+ogame-ninja@users.noreply.github.com>
2024-07-14 16:53:34 -0700

    Create repat_att_ships.go

---
+++ official/go_vm/repat_att_ships.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "ogame/pkg/ogame"
+    "nja"
+)
+
+func main() {
+    masterCoord := "1:2:3"
+
+    //------------------------------------------------------------------------------
+    
+    attShips := []ogame.ID{nja.LIGHTFIGHTER, nja.HEAVYFIGHTER, nja.CRUISER,
+        nja.BATTLESHIP, nja.BOMBER, nja.DESTROYER, nja.DEATHSTAR, nja.BATTLECRUISER}
+    
+    celestials := nja.GetCachedCelestials()
+    for _, celestial := range celestials {
+        ships, _ := celestial.GetShips()
+        fleet := nja.NewFleet()
+        fleet.SetOrigin(celestial)
+        fleet.SetDestination(masterCoord)
+        fleet.SetMission(nja.PARK)
+        for _, shipID := range attShips {
+            fleet.AddShips(shipID, ships.ByID(shipID))
+        }
+        f, err := fleet.SendNow()
+        if err != nil {
+            nja.LogError(err)
+        } else {
+            nja.Print(f)
+        }
+    }
+}
