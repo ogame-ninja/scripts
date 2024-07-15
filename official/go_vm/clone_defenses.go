ce98abfea4310df9cd88b4aa47616b5b31a521c4
ogame-ninja <45097930+ogame-ninja@users.noreply.github.com>
2024-07-14 19:08:34 -0700

    Create clone_defenses.go

---
+++ official/go_vm/clone_defenses.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "ogame/pkg/ogame"
+    "nja"
+)
+
+func main() {
+    master := nja.GetCachedCelestial("1:106:10")
+    slave := nja.GetCachedCelestial("1:103:11")
+    
+    for {
+        masterDef, _ := master.GetDefense()
+        slaveDef, _ := slave.GetDefense()
+        prodQueue, _, _ := slave.GetProduction() // get build queue
+        
+        // DefencesArr is a built-in array that contains all the defense entity IDs
+        for _, defID := range nja.DefencesArr {
+            delta := masterDef.ByID(defID) - slaveDef.ByID(defID)
+            
+            // Remove defenses that are already in build queue
+            for _, item := range prodQueue {
+                if item.ID == defID {
+                    delta -= item.Nbr
+                }
+            }
+            
+            if delta > 0 {
+                slave.BuildDefense(defID, delta)
+                nja.Print("Build", delta, defID) // eg: Build 12 RocketLauncher
+            }
+        }
+        
+        nja.Sleep(int64(60 * 60 * 1000)) // Check again in 1 hour
+    }
+}
