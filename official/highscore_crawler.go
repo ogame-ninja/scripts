36dd58a0e0b3439f1c3b9cadf480ba279be46400
ogame-ninja <45097930+ogame-ninja@users.noreply.github.com>
2025-02-01 02:27:45 -0800

    Update highscore_crawler.go

--- official/highscore_crawler.go
+++ official/highscore_crawler.go
@@ -5,7 +5,7 @@ if err != nil {
     Printf("Error retrieving the initial ranking page: %v", err)
     return
 }
-
+Printf("Page %d: %d players captured", 1, len(highscore.Players))
 for page = 2; page <= highscore.NbPage; page++ {
     SleepRandMs(100, 200)
     highscore, err = GetHighscore(1, 1, page)
@@ -13,6 +13,5 @@ for page = 2; page <= highscore.NbPage; page++ {
         Printf("Error retrieving ranking from page %d: %v", page, err)
         continue
     }
-    numPlayers = len(highscore.Players)
-    Printf("Page %d: %d players captured", page, numPlayers)
+    Printf("Page %d: %d players captured", page, len(highscore.Players))
 }
